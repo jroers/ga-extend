60246f37ea1a87b810c90521dfbffbbb38eafe88
chan-d <chan.danny516@gmail.com>
2016-04-18 14:38:04 -0700

    sample seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+user1_data = {
+	first: 'test',
+	last: 'user',
+	email: 'test@user.com',
+	password: '123',
+	program: 'WDI',
+	cohort: '25',
+	current_title: 'Grand Master'
+}
+user1= User.create(user1_data)
+
+card1_data = {
+	family: '',
+	title: '',
+	working_on: '',
+}
+card1 = Card.create(card1_data)
+
+

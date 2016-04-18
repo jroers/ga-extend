4d89dae9f7f2c6e1494f524c9e8f4700ff54439c
chan-d <chan.danny516@gmail.com>
2016-04-18 12:28:41 -0700

    schema created for cards table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418185706) do
+ActiveRecord::Schema.define(version: 20160418190456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "family"
+    t.string   "title"
+    t.text     "working_on"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "cards", ["user_id"], name: "index_cards_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first"
@@ -28,4 +39,5 @@ ActiveRecord::Schema.define(version: 20160418185706) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "cards", "users"
 end

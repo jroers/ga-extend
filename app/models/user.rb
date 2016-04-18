bf17f6d9ae1d2c5791c831e06a9aa8569d82b872
jroers <joe.roers@gmail.com>
2016-04-18 14:54:15 -0700

    included users to skills and skills to users via models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
+	has_many :user_skills
+	has_many :skills, through: :user_skills
+
 	def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])

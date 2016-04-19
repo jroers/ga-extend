b63e297b1184d5e7ad950208ec3c8a3c577eceeb
chan-d <chan.danny516@gmail.com>
2016-04-19 12:48:55 -0700

    nav bar working

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/sessions', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 
 end

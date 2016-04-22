5555f80fafead877910386c3a2284c629d48d11a
jroers <joe.roers@gmail.com>
2016-04-22 14:20:14 -0700

    removed sanity checks

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-	console.log("JS is working");
-
 	$cards = $(".cards .container");
 	$networking = $(".container.networking");
 	$project = $(".container.project");
@@ -8,28 +6,24 @@ $(document).ready(function() {
 
 	$(".btn-filter.btn-networking").click(function(e) {
 		e.preventDefault();
-		console.log("click!");
 		$cards.remove();
 		$(".cards").append($networking);
 	});
 
 	$(".btn-filter.btn-project").click(function(e) {
 		e.preventDefault();
-		console.log("click!");
 		$cards.remove();
 		$(".cards").append($project);
 	});
 
 	$(".btn-filter.btn-skill-share").click(function(e) {
 		e.preventDefault();
-		console.log("click!");
 		$cards.remove();
 		$(".cards").append($skillShare);
 	});
 
 	$(".btn-filter.btn-all").click(function(e) {
 		e.preventDefault();
-		console.log("click!");
 		$cards.remove();
 		$(".cards").append($cards);
 	});

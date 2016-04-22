ee5b28d64c33e9315d7db948325351b8ca987656
jroers <joe.roers@gmail.com>
2016-04-22 14:14:00 -0700

    jk, networking filter now works for reals

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 	console.log("JS is working");
+
+	$cards = $(".cards .container");
+	$networking = $(".container.networking");
+	$project = $(".container.project");
+	$skillShare = $(".container.skill_share");
+
+	$(".btn-filter.btn-networking").click(function(e) {
+		e.preventDefault();
+		console.log("click!");
+		$cards.remove();
+		$(".cards").append($networking);
+	});
+
 });

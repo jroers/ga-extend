3e75aa60875cc9f4040ac45ef8cf379213df1878
jroers <joe.roers@gmail.com>
2016-04-22 14:16:04 -0700

    All 4 filters work

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -13,4 +13,24 @@ $(document).ready(function() {
 		$(".cards").append($networking);
 	});
 
+	$(".btn-filter.btn-project").click(function(e) {
+		e.preventDefault();
+		console.log("click!");
+		$cards.remove();
+		$(".cards").append($project);
+	});
+
+	$(".btn-filter.btn-skill-share").click(function(e) {
+		e.preventDefault();
+		console.log("click!");
+		$cards.remove();
+		$(".cards").append($skillShare);
+	});
+
+	$(".btn-filter.btn-all").click(function(e) {
+		e.preventDefault();
+		console.log("click!");
+		$cards.remove();
+		$(".cards").append($cards);
+	});
 });

c950f8f4d510ecbe80069cdcadf58b7f638f57ed
jroers <joe.roers@gmail.com>
2016-04-24 12:47:20 -0700

    JS file toggles active class for buttons. TODO: Style .active appropriately to indicate currently selected button

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -8,26 +8,42 @@ function renderCards() {
 		e.preventDefault();
 		$cards.remove();
 		$(".cards").append($networking);
+		$(".btn-filter").removeClass("active");
+		$(this).addClass("active");
 	});
 
 	$(".btn-filter.btn-project").click(function(e) {
 		e.preventDefault();
 		$cards.remove();
 		$(".cards").append($project);
+		$(".btn-filter").removeClass("active");
+		$(this).addClass("active");
 	});
 
 	$(".btn-filter.btn-skill-share").click(function(e) {
 		e.preventDefault();
 		$cards.remove();
 		$(".cards").append($skillShare);
+		$(".btn-filter").removeClass("active");
+		$(this).addClass("active");
 	});
 
 	$(".btn-filter.btn-all").click(function(e) {
 		e.preventDefault();
 		$cards.remove();
 		$(".cards").append($cards);
+		$(".btn-filter").removeClass("active");
+		$(this).addClass("active");
 	});
 }
+
+function assignActiveClass() {
+	console.log("click");
+	$(".btn-filter").removeClass("active");
+	$(this).addClass("active");
+	console.log($(this));
+}
+
 
 $(document).on('page:load', renderCards);
 
